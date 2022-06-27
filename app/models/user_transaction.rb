04cc900cca00920104df1fd29241bf25e72b23e5
Shweta Yadav <shwetay224@gmail.com>
2022-06-28 00:19:28 +0530

    reverted validation from user transaction model

--- app/models/user_transaction.rb
+++ app/models/user_transaction.rb
@@ -1,5 +1,5 @@
 class UserTransaction < ApplicationRecord
-    validates :user_id,:allow_blank => false
-    validates :expense_id,:allow_blank => false
-    validates :paid_amount,:allow_nil => false
+    # validates :user_id, allow_nil: false
+    # validates :expense_id, allow_nil: false
+    # validates :paid_amount, allow_nil: false
 end
